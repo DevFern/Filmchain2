d306c2bab64bf0f7d77c6b439829d446a9da25db
DevFern <fernandes_2@hotmail.com>
2025-03-07 13:33:06 -0500

    Create ChainSelector.js

---
+++ components/ChainSelector.js
@@ -0,0 +1,11 @@
+// components/ChainSelector.js
+export default function ChainSelector() {
+  const chains = [
+    { id: 1, name: 'Ethereum', icon: '/images/ethereum.svg' },
+    { id: 56, name: 'Binance Smart Chain', icon: '/images/binance.svg' },
+    { id: 137, name: 'Polygon', icon: '/images/polygon.svg' },
+    { id: 43114, name: 'Avalanche', icon: '/images/avalanche.svg' }
+  ];
+  
+  // Chain selection interface with bridge functionality
+}
